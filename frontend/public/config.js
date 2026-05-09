ce4763cd47a660d13b0b5dbf2b760f027a88a6c0
duc15112003 <ducpm15112003@gmail.com>
2026-05-10 00:17:34 +0700

    deploy prod

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,3 +1,4 @@
 window.__APP_CONFIG__ = {
-  API_ORIGIN: 'http://localhost:8080',
+  // API_ORIGIN: 'http://localhost:8080',
+  API_ORIGIN: 'http://160.250.181.24:8080',
 };
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+// const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://160.250.181.24:8080/api';
 
 export default API_BASE_URL;
